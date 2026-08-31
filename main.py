b41e4a596873acdfd36111ddb94426da7ac6d5d8
David <davidviaxespam@gmail.com>
2026-08-31 12:40:56 -0700

    added top 5 total points

--- main.py
+++ main.py
@@ -4,4 +4,15 @@ csv_path = 'data/nba_player_stats_2026.csv'
 
 df = pd.read_csv(csv_path)
 
-print(df.head())
+print("NBA 2025-26 SEASON")
+
+print("Top 5 in total points scored:")
+
+df_total_pts = df.sort_values(by="PTS", ascending=False)
+
+for i in range(0, 5):
+    player_name = df_total_pts["PLAYER"].iloc[i]
+
+    points_scored = df_total_pts["PTS"].iloc[i]
+
+    print(f"{i + 1}: {player_name} scored a total of {points_scored} points")
